93256e46c3ccff0ba0dfeb6f51672a4aaca8de3f
Bohdan Prokopenko <bohdan.prokopenko@hotmail.com>
2021-08-13 10:49:33 +0300

    Provided minor refactoring

--- js/script.js
+++ js/script.js
@@ -7,21 +7,23 @@ $(document).ready(
 
 		let followTxt = 'Follow';
 
-		let followingTxt = 'Following';
-
 		function format(number) {
 			return number.toString()
 				.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 		}
 
 		function toogleFollowing() {
-			$('#followBtn').css('background-color','#34CF7A');
-		    $('#followBtn').html('<div class="icon-ok"></div> Following');
+			following = true;
+			followers++;
+			$('#followBtn').css('background-color', '#34CF7A');
+			$('#followBtn').html('<div class="icon-ok"></div> Following');
 		}
 
 		function toogleFollow() {
+			following = false;
+			followers--;
 			$('#followBtn').removeClass('icon-ok');
-			$('#followBtn').css('background-color','#2589cc');
+			$('#followBtn').css('background-color', '#2589cc');
 			$('#followBtn').html(followTxt);
 		}
 
@@ -30,13 +32,9 @@ $(document).ready(
 		$('.follow').on("click", function () {
 
 			if (!following) {
-				following = true;
-				followers++;
 				toogleFollowing();
 			}
 			else {
-				following = false;
-				followers--
 				toogleFollow()
 			}
 			$('.counter').html(format(followers));
