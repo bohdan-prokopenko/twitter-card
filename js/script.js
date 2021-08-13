954502b24a46c0c97a286f147dfe4e0ecaf5266b
Bohdan Prokopenko <bohdan.prokopenko@hotmail.com>
2021-08-13 10:49:00 +0300

    Added functions to swirch state of the follow btn

--- js/script.js
+++ js/script.js
@@ -1,10 +1,45 @@
 $(document).ready(
+	function setup() {
 
-	function iniciar(){
-	$('.follow').on("click", function(){
-		$('.follow').css('background-color','#34CF7A');
-		$('.follow').html('<div class="icon-ok"></div> Following');
-		$('.counter').html('100,501');
-		}); 
-	}
+		let followers = 100500;
+
+		let following = false;
+
+		let followTxt = 'Follow';
+
+		let followingTxt = 'Following';
+
+		function format(number) {
+			return number.toString()
+				.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		}
+
+		function toogleFollowing() {
+			$('#followBtn').css('background-color','#34CF7A');
+		    $('#followBtn').html('<div class="icon-ok"></div> Following');
+		}
+
+		function toogleFollow() {
+			$('#followBtn').removeClass('icon-ok');
+			$('#followBtn').css('background-color','#2589cc');
+			$('#followBtn').html(followTxt);
+		}
+
+		$('.counter').html(format(followers));
+
+		$('.follow').on("click", function () {
+
+			if (!following) {
+				following = true;
+				followers++;
+				toogleFollowing();
+			}
+			else {
+				following = false;
+				followers--
+				toogleFollow()
+			}
+			$('.counter').html(format(followers));
+		});
+	},
 );
